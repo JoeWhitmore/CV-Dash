59ff3958f5d908039421c3aec52f559b6111fae0
JoeWhitmore <Joew@carevicinity.com>
2026-05-14 18:11:00 +1000

    Add mock data barrel export

---
+++ lib/mock/index.ts
@@ -0,0 +1,4 @@
+export { burndownBySprint } from "./burndown";
+export { currentSprintId, sprintById, sprints } from "./sprints";
+export { team, teamById } from "./team";
+export { ticketByKey, tickets } from "./tickets";
